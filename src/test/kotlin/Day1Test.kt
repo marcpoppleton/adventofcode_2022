44d41c4437b4e11ad8870f74f409a6ebce5ac6a8
Marc Poppleton <marc.poppleton@gmail.com>
2022-12-02 16:28:10 +0100

    Added run results to test cases

--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -4,19 +4,34 @@ import org.junit.jupiter.api.Assertions.*
 
 class Day1Test {
 
-    val expectedResultFor1 = 24000L
-    val expectedResultFor2 = 45000L
+    val expectedResultForSample1 = 24000L
+    val expectedResultForSample2 = 45000L
+    val expectedResultFor1 = 71924L
+    val expectedResultFor2 = 210406L
 
     @Test
-    fun part1() {
+    fun partsample1() {
         val fileEntries = Day1().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/test/resources/Day1.txt")
+        val result = Day1().part1(fileEntries)
+        assertEquals(expectedResultForSample1,result)
+    }
+
+    @Test
+    fun partsample2() {
+        val fileEntries = Day1().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/test/resources/Day1.txt")
+        val result = Day1().part2(fileEntries)
+        assertEquals(expectedResultForSample2,result)
+    }
+
+    fun part1() {
+        val fileEntries = Day1().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/main/resources/Day1.txt")
         val result = Day1().part1(fileEntries)
         assertEquals(expectedResultFor1,result)
     }
 
     @Test
     fun part2() {
-        val fileEntries = Day1().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/test/resources/Day1.txt")
+        val fileEntries = Day1().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/main/resources/Day1.txt")
         val result = Day1().part2(fileEntries)
         assertEquals(expectedResultFor2,result)
     }
--- src/test/kotlin/Day2Test.kt
+++ src/test/kotlin/Day2Test.kt
@@ -4,19 +4,35 @@ import org.junit.jupiter.api.Assertions.*
 
 class Day2Test {
 
-    val expectedResultFor1 = 15L
-    val expectedResultFor2 = 12L
+    val expectedResultForSample1 = 15L
+    val expectedResultForSample2 = 12L
+    val expectedResultFor1 = 12772L
+    val expectedResultFor2 = 11618L
+
+    @Test
+    fun partSample1() {
+        val fileEntries = Day2().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/test/resources/Day2.txt")
+        val result = Day2().part1(fileEntries)
+        assertEquals(expectedResultForSample1,result)
+    }
+
+    @Test
+    fun partSample2() {
+        val fileEntries = Day2().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/test/resources/Day2.txt")
+        val result = Day2().part2(fileEntries)
+        assertEquals(expectedResultForSample2,result)
+    }
 
     @Test
     fun part1() {
-        val fileEntries = Day2().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/test/resources/Day2.txt")
+        val fileEntries = Day2().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/main/resources/Day2.txt")
         val result = Day2().part1(fileEntries)
         assertEquals(expectedResultFor1,result)
     }
 
     @Test
     fun part2() {
-        val fileEntries = Day2().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/test/resources/Day2.txt")
+        val fileEntries = Day2().getEntries("/Users/marcpoppleton/Code/Adventofcode_2022/src/main/resources/Day2.txt")
         val result = Day2().part2(fileEntries)
         assertEquals(expectedResultFor2,result)
     }
